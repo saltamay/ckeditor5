aeeeec68d6c6454674ea06d2a5d4488ae615a85b
Sal Tamay <sal@logicaldox.com>
2020-09-11 18:17:42 -0400

    Remove all the other packages

--- docs/_snippets/features/mathtype.js
+++
@@ -1,6 +0,0 @@
-/**
- * @license Copyright (c) 2003-2020, CKSource - Frederico Knabben. All rights reserved.
- * For licensing, see LICENSE.md or https://ckeditor.com/legal/ckeditor-oss-license
- */
-
-/* Content removed as a workaround in #7944. Should be restored once the upstream is fixed. */
--- packages/ckeditor5-theme-lark/tests/manual/iconset.md
+++
@@ -1,7 +0,0 @@
-## Project icons
-
-Make sure all icons in the toolbar render correctly.
-
-## Notes
-
-* Some icons like "marker" and "pen" should be filled with yellow color.
